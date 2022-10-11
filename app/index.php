b2b1d86897fd43d00eb57686362758d1994c8047
Adriaya63 <Adriaya63@gmail.com>
2022-10-11 19:00:40 +0200

    Preparando diseño

--- app/index.php
+++ app/index.php
@@ -1,29 +1,14 @@
-<?php
-  echo '<h1>Yeah, it works!<h1>';
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
-
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-  }
+<?php include("templates/header.php")?>
 
 
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-   </tr>";
-   
-
-}
-
-?>
+  
+      <div class="jumbotron">
+        <h1 class="display-3">Jumbo heading</h1>
+        <p class="lead">Jumbo helper text</p>
+        <hr class="my-2">
+        <p>More info</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        </p>
+      </div>
+<?php include("templates/footer.php")?>
---
+++ app/nosotros.php
@@ -0,0 +1,10 @@
+<?php include("templates/header.php")?>
+
+<div class="jumbotron">
+    <h1 class="display-3">¿Quienes Somos?</h1>
+    <p class="lead">Somos un grupo de la asignatura de SGSSI de la carrera de Ingenieria Informatica de Gestión y Sistemas de Información de la UPV</p>
+    <hr class="my-2">
+    
+</div>
+
+<?php include("templates/footer.php")?>
