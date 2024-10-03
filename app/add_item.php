adee3574d1dbbdd8a4e4115b845a16a838ee1be1
AnderCorral <ander.corral2002@gmail.com>
2024-10-03 16:15:49 +0200

    d

--- app/add_item.php
+++ app/add_item.php
@@ -1,11 +1,14 @@
 <form id="item_add_form" action="procesar_add_item.php" method="POST">
+    <label for="id">Título:</label>
+    <input type="text" id="id" name="id" required>
+    
     <label for="titulo">Título:</label>
     <input type="text" id="titulo" name="titulo" required>
     
     <label for="anio">Año:</label>
     <input type="number" id="anio" name="anio" required>
     
-    <label for="artista">Artista:</label> <input type="text" id="artista" name="artista" required>  
+    <label for="director">Director:</label> <input type="text" id="director" name="director" required>  
     <label for="genero">Género:</label>
 <input type="text" id="genero" name="genero" required>
 
