47327efbcaad7e88be25799f29895423298d99a2
AnderCorral <ander.corral2002@gmail.com>
2024-10-03 16:00:25 +0200

    a

--- database.sql
+++ database.sql
@@ -16,6 +16,10 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `usuarios` (`nombre`, `apellidos`, `dni`, `telefono`, `fecha_nacimiento`, `email`, `username`, `password`) VALUES
+('Juan', 'Pérez', '12345678-Z', '600123456', '1990-05-10', 'juan.perez@example.com', 'juanp', MD5('password123')),
+('Ana', 'García', '87654321-X', '600987654', '1985-11-22', 'ana.garcia@example.com', 'anag', MD5('mypassword'));
+
 -- Crear la tabla para los ítems (por ejemplo, películas)
 CREATE TABLE IF NOT EXISTS `items` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
