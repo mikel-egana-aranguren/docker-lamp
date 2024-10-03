f6d9123455edd1fd09d0b0736c7741a160ad8159
Aitzol Luengo <luengoaitzol@gmail.com>
2024-10-03 16:30:43 +0200

    he empezado con el codigo del login

---
+++ app/login.php
@@ -0,0 +1,15 @@
+<?php
+	include 'databaseConnect.php';
+	
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+		$erabiltzaile = $_POST['erabiltzaile'];
+		$pasahitz = $_post['pasahitz'];
+	
+	
+
+
+
+	
+	
+	}
