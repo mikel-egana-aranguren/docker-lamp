bc4bc5a741b1aa8cae0a564f7e5f38aaf2ca007e
AnderCorral <ander.corral2002@gmail.com>
2024-10-03 15:37:45 +0200

    corr

--- app/conexion.php
+++ app/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "db"; // Nombre del servicio de MariaDB en Docker
-$username = "root"; // Usuario por defecto
-$password = "your_password"; // Cambia esto por tu contraseña
-$dbname = "nombre_de_la_base_de_datos"; // Nombre de tu base de datos
+$username = "admin"; // Usuario por defecto
+$password = "test"; // Cambia esto por tu contraseña
+$dbname = "basededatos"; // Nombre de tu base de datos
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
