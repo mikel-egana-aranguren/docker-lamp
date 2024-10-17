08e032d57987608665aced06008575494708c2f6
AsierMonge3 <131005875+AsierMonge3@users.noreply.github.com>
2024-10-17 11:23:51 +0200

    cambio

--- app/index.php
+++ app/index.php
@@ -15,7 +15,7 @@
     <h1>Bideojokoen Zerrenda</h1>
     <div class="bideojoko-zerrenda">
        <?php
-        $sql = "SELECT * FROM bideojokoak";
+        $sql = "SELECT * FROM bideojokoa";
         $result = $konexioa->query($sql);
 
         if (!$result) {
