845a4efe4583eb294ab9ff7ca3171ae5aa75e191
ikerfernandezmolano <ikerfernandezmolano@gmail.com>
2025-10-13 12:50:03 +0200

    Commit_1

--- app/register.php
+++ app/register.php
@@ -1,34 +1,16 @@
 <?php
   echo '<form id="registerForm">
-<h1 style="color: #5e9ca0;"><span style="text-decoration: underline; color: #003366;">Registrarse</span></h1>
-<p style="text-align: justify; padding-left: 40px;"><label>Usuario:</label> <input id="user" name="user" required="" type="text" /></p>
+<h1 style="color: #5e9ca0;"><strong><span style="text-decoration: underline; color: #003366;">Registrarse</span></strong></h1>
+<p><span style="text-decoration: underline; color: #003366;"></span></p>
+<p style="text-align: justify; padding-left: 40px;"><label>Nombre:</label> <input id="user" name="user" required="" type="text" /></p>
+<p style="text-align: justify; padding-left: 40px;"><label>Apellidos:</label> <input id="user" name="user" required="" type="text" /></p>
+<p style="text-align: justify; padding-left: 40px;"><label>DNI:</label> <input id="user" name="user" required="" type="text" /></p>
+<p style="text-align: justify; padding-left: 40px;"><label>Tel&eacute;fono:</label> <input id="user" name="user" required="" type="text" /></p>
+<p style="text-align: justify; padding-left: 40px;"><label>Fecha de Nacimiento:</label> <input id="user" name="user" required="" type="text" /></p>
+<p style="text-align: justify; padding-left: 40px;"><label>Correo:</label> <input id="user" name="user" required="" type="text" /></p>
 <p style="text-align: justify; padding-left: 40px;"><label>Contrase&ntilde;a:</label> <input id="user" name="user" required="" type="text" /></p>
-<p id="mensaje" style="text-align: justify; padding-left: 40px;"></p>
+<p style="text-align: justify; padding-left: 40px;"><label>Repetir contrase&ntilde;a:</label> <input id="user" name="user" required="" type="text" /></p>
+<p style="text-align: justify; padding-left: 40px;"></p>
+<div style="text-align: center;"><button id="register_submit">Confirmar</button></div>
 </form>';
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
-
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-  }
-
-
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-   </tr>";
-   
-
-}
-
 ?>
