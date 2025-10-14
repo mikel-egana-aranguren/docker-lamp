e46fe8922f5d5a834453df71702b511d66fc4a20
AdrianV3 <avisan2000@gmail.com>
2025-10-15 00:16:21 +0200

    pagina inicio v1

--- app/index.php
+++ app/index.php
@@ -1,5 +1,20 @@
+<html>
+   <head>
+    <link rel="stylesheet" type="text/css" href="inicioStyle.css">
+   </head>
+   <body>
+    <h1>¡Bienvenid@ a SafeFilms!<h1>
+    <h2>Consulta nuestro catálogo con lo mejor del cine: <h2>
+    <form>
+     <button class="ctlg">Catálogo</button>
+     <button class="inic">Iniciar sesión</button>
+     <button class="reg">Registrarse</button>
+    </form>
+   </body>
+  </html>
+  
 <?php
-  echo '<h1>Yeah, it works!<h1>';
+  
   // phpinfo();
   $hostname = "db";
   $username = "admin";
@@ -13,15 +28,15 @@
 
 
 
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
+$query = mysqli_query($conn, "SELECT * FROM pelicula")
    or die (mysqli_error($conn));
 
 while ($row = mysqli_fetch_array($query)) {
   echo
    "<table border='1'>
      <tr>
-      <td>{$row['id']}</td>
-      <td>{$row['nombre']}</td>
+      <td>{$row['titulo']}</td>
+      <td>{$row['duracion']}</td>
      </tr>
     </table>";
    
