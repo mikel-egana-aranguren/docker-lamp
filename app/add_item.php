5fd7d28986316334959c154f90e77fc1702096f1
nahiagalvan <nahiagalvan@gmail.com>
2025-10-16 12:30:34 +0200

    Commit-4

--- app/add_item.php
+++ app/add_item.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 // --- CONFIGURACIÓN DE LA BASE DE DATOS ---
 $hostname = "db";        // o "localhost" si usas XAMPP
 $username = "admin";     // tu usuario
